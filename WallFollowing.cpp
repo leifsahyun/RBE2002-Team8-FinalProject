e58c4f19eb4dfb04980ba40bd2538bda602b38b8
leifsahyun <33590091+leifsahyun@users.noreply.github.com>
2017-11-12 16:23:41 -0500

    Add files via upload

---
+++ WallFollowing.cpp
@@ -0,0 +1,14 @@
+#include "WallFollowing.h"
+
+void WallFollowing::setup(){
+  control = new PidControl(&scaledUltraReading, &motorDiff, (long)(wallDistance*scalingFactor));
+}
+
+void WallFollowing::update(){
+  ultra->trigger();
+  scaledUltraReading = scalingFactor*(ultra->getLastDist());
+  control->updatePid();
+  leftServo->write(150+motorDiff);
+  rightServo->write(150-motorDiff);
+}
+
---
+++ WallFollowing.h
@@ -0,0 +1,18 @@
+#include "Servo.h"
+#include "PidControl.h"
+#include "Ultrasonic.h"
+#include "Arduino.h"
+
+namespace WallFollowing {
+  int wallDistance = 4;
+  const int scalingFactor = 1000;
+  long scaledUltraReading = 0;
+  int motorDiff = 0;
+  Servo *leftServo;
+  Servo *rightServo;
+  Ultrasonic *ultra;
+  PidControl *control;
+  void setup();
+  void update();
+}
+
