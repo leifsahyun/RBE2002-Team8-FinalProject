dc5947be64a18dfb824650096274f7cba1d207a9
leifsahyun <33590091+leifsahyun@users.noreply.github.com>
2017-11-16 18:06:14 -0500

    Add files via upload

--- finalProjectCode.ino
+++ finalProjectCode.ino
@@ -1,12 +1,116 @@
 #include "WallFollowing.h"
 #include "OdometryProject.h"
+enum State
+  {Calibration,
+  WallFollowing,
+  TurnRight,
+  TurnLeft,
+  TurnToFlame,
+  ApproachFlame,
+  FanOn,
+  TurnToStart,
+  MoveForward,
+  TurnToWall,
+  WallFollowingStage2,
+  TurnLeftStage2,
+  Finish};
+
+State currentState = Calibration;
+State lastState = Calibration;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // switch block for one-time code on entering each state:
+  if(lastState != currentState){
+    switch (currentState)
+    {
+      case Calibration:
 
+      break;
+      case WallFollowing:
+
+      break;
+      case TurnRight:
+
+      break;
+      case TurnLeft:
+
+      break;
+      case TurnToFlame:
+
+      break;
+      case ApproachFlame:
+
+      break;
+      case FanOn:
+
+      break;
+      case TurnToStart:
+
+      break;
+      case MoveForward:
+
+      break;
+      case TurnToWall:
+
+      break;
+      case WallFollowingStage2:
+
+      break;
+      case TurnLeftStage2:
+
+      break;
+      case Finish:
+
+      break;
+    }
+  }
+  //switch block for continuous code running during each state:
+  switch (currentState)
+    {
+      case Calibration:
+
+      break;
+      case WallFollowing:
+
+      break;
+      case TurnRight:
+
+      break;
+      case TurnLeft:
+
+      break;
+      case TurnToFlame:
+
+      break;
+      case ApproachFlame:
+
+      break;
+      case FanOn:
+
+      break;
+      case TurnToStart:
+
+      break;
+      case MoveForward:
+
+      break;
+      case TurnToWall:
+
+      break;
+      case WallFollowingStage2:
+
+      break;
+      case TurnLeftStage2:
+
+      break;
+      case Finish:
+
+      break;
+    }
+    lastState = currentState;
+    currentState++;
 }
